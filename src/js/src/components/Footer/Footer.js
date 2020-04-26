17504d12b5d485506bdb45f28ddaae160c0c31f3
n1523255 <kyle.krupp@libertymutual.com>
2020-04-26 17:43:49 -0400

    form refactored to hooks

--- src/js/src/components/Footer/Footer.js
+++ src/js/src/components/Footer/Footer.js
@@ -1,47 +1,50 @@
-import React, { useContext, useState } from 'react';
-import Container from '../../Container';
-import AddPlayerModal from '../modals/AddPlayerModal'
-import { Button, Avatar, Modal } from 'antd';
-import AddPlayerForm from '../forms/AddPlayerForm'
-import './Footer.css';
-import { GlobalContext } from '../../context/GlobalState';
-import { errorNotification } from '../Notification';
+import React, { useContext, useState } from "react";
+import Container from "../../Container";
+import { Button, Avatar, Modal } from "antd";
+import AddPlayerForm from "../forms/AddPlayerForm";
+import "./Footer.css";
+import { GlobalContext } from "../../context/GlobalState";
+import { errorNotification } from "../Notification";
 
+const Footer = () => {
+  const { players, getPlayers } = useContext(GlobalContext);
+  const [isModalVisible, toggleModal] = useState(false);
 
-const Footer = () =>  {
-    const { players, getPlayers } = useContext(GlobalContext);
-    const [isModalVisible, toggleModal] = useState(false);
-
-    return (
-    <div className='footer'>
-       <Container>
-            {players.length !== undefined ?
-                <Avatar 
-                    style={{backgroundColor: '#f56a00', marginRight: '5px'}}
-                    size='large'>{players.length}</Avatar> : null
-            }
-            <Button onClick={() => toggleModal(!isModalVisible)} type='primary'>Add new player +</Button>
-        </Container>
-        <Modal
-            title='Add new player'
-            visible={isModalVisible}
-            onOk={() => toggleModal(!isModalVisible)}
-            onCancel={() => toggleModal(!isModalVisible)}
-            width={1000}>
-            <AddPlayerForm
-              onSuccess={() => {
-                toggleModal(!isModalVisible); 
-                getPlayers();
-              }}
-              onFailure={(error) => {
-                const message = error.error.message;
-                const description = error.error.httpStatus;
-                errorNotification(message, description);
-              }}
-            />
-          </Modal>
+  return (
+    <div className="footer">
+      <Container>
+        {players.length !== undefined ? (
+          <Avatar
+            style={{ backgroundColor: "#f56a00", marginRight: "5px" }}
+            size="large"
+          >
+            {players.length}
+          </Avatar>
+        ) : null}
+        <Button onClick={() => toggleModal(!isModalVisible)} type="primary">
+          Add new player +
+        </Button>
+      </Container>
+      <Modal
+        title="Add new player"
+        visible={isModalVisible}
+        onOk={() => toggleModal(!isModalVisible)}
+        onCancel={() => toggleModal(!isModalVisible)}
+        width={1000}
+      >
+        <AddPlayerForm
+          onSuccess={() => {
+            toggleModal(!isModalVisible);
+            getPlayers();
+          }}
+          onFailure={(error) => {
+            const message = error.error.message;
+            const description = error.error.httpStatus;
+            errorNotification(message, description);
+          }}
+        />
+      </Modal>
     </div>
-);
-
-}
+  );
+};
 export default Footer;
--- src/js/src/components/forms/AddPlayerForm.js
+++ src/js/src/components/forms/AddPlayerForm.js
@@ -1,49 +1,67 @@
-import React from 'react';
-import { Formik } from 'formik';
-import { Input, Button, Tag } from 'antd';
-// import { addNewPlayer } from '../client';
+import React, { useContext } from "react";
+import { Formik } from "formik";
+import { Input, Button, Tag } from "antd";
+import { GlobalContext } from "../../context/GlobalState";
 
-const inputBottomMargin = {marginBottom: '10px'};
-const tagStyle = {backgroundColor: '#f50', color: 'white', ...inputBottomMargin};
+const inputBottomMargin = { marginBottom: "10px" };
+const tagStyle = {
+  backgroundColor: "#f50",
+  color: "white",
+  ...inputBottomMargin,
+};
 
-const AddPlayerForm = (props) => (
+const AddPlayerForm = (props) => {
+  const { addPlayer } = useContext(GlobalContext);
+
+  return (
     <Formik
-        initialValues={{ firstName: '', lastName: '', battingAverage: '', playerType: ''}}
-        validate={values => {
-            let errors = {};
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        battingAverage: "",
+        playerType: "",
+      }}
+      validate={(values) => {
+        let errors = {};
 
-            if (!values.firstName) {
-                errors.firstName = 'First Name Required'
-            }
+        if (!values.firstName) {
+          errors.firstName = "First Name Required";
+        }
 
-            if (!values.lastName) {
-                errors.lastName = 'Last Name Required'
-            }
+        if (!values.lastName) {
+          errors.lastName = "Last Name Required";
+        }
 
-            if (!values.battingAverage) {
-                errors.battingAverage = 'Batting Average Required';
-            }
+        if (!values.battingAverage) {
+          errors.battingAverage = "Batting Average Required";
+        }
 
-            if (!values.playerType) {
-                errors.playerType = 'Player Type Required';
-            } else if (!['HITTER', 'hitter', 'PITCHER', 'pitcher'].includes(values.playerType)) {
-                errors.playerType = 'Player type must be a pitcher or a hitter';
-            }
-            
-            return errors;
-        }}
-        onSubmit={(player, { setSubmitting }) => {
-            // addNewPlayer(player).then(() => {
-            //     props.onSuccess();
-            // })
-            // .catch(err => {
-            //     props.onFailure(err);
-            // })
-            // .finally(() => {
-            //     setSubmitting(false);
-            // })
-        }}>
-    {({
+        if (!values.playerType) {
+          errors.playerType = "Player Type Required";
+        } else if (
+          !["HITTER", "hitter", "PITCHER", "pitcher"].includes(
+            values.playerType
+          )
+        ) {
+          errors.playerType = "Player type must be a pitcher or a hitter";
+        }
+
+        return errors;
+      }}
+      onSubmit={(player, { setSubmitting }) => {
+        addPlayer(player)
+          .then(() => {
+            props.onSuccess();
+          })
+          .catch((err) => {
+            props.onFailure(err);
+          })
+          .finally(() => {
+            setSubmitting(false);
+          });
+      }}
+    >
+      {({
         values,
         errors,
         touched,
@@ -52,61 +70,66 @@ const AddPlayerForm = (props) => (
         handleSubmit,
         isSubmitting,
         submitForm,
-        isValid
+        isValid,
         /* and other goodies */
-    }) => (
+      }) => (
         <form onSubmit={handleSubmit}>
-            <Input
-                style={inputBottomMargin}
-                name="firstName"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.firstName}
-                placeholder='First name e.g. Mike'
-            />
-            {errors.firstName && touched.firstName &&
-                    <Tag style={tagStyle}>{errors.firstName}</Tag>}
-            <Input
-                style={inputBottomMargin}
-                name="lastName"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.lastName}
-                placeholder='Last name e.g. Trout'
-            />
-            {errors.lastName && touched.lastName && 
-                <Tag style={tagStyle}>{errors.lastName}</Tag>}
-            <Input
-                style={inputBottomMargin}
-                name="battingAverage"
-                type='Batting Average'
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.battingAverage}
-                placeholder='Batting Average eg. "300"'
-            />
-            {errors.battingAverage && touched.battingAverage &&
-                <Tag style={tagStyle}>{errors.battingAverage}</Tag>}
-            <Input
-                style={inputBottomMargin}
-                name="playerType"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.playerType}
-                placeholder='Player Type eg. "HITTER" or "PITCHER"'
-            />
-            {errors.playerType && touched.playerType &&
-                <Tag style={tagStyle}>{errors.playerType}</Tag>}
-            <Button 
-                onClick={() => submitForm()} 
-                type="submit" 
-                disabled={isSubmitting | (touched && !isValid)}>
-                Submit
-            </Button>
+          <Input
+            style={inputBottomMargin}
+            name="firstName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.firstName}
+            placeholder="First name e.g. Mike"
+          />
+          {errors.firstName && touched.firstName && (
+            <Tag style={tagStyle}>{errors.firstName}</Tag>
+          )}
+          <Input
+            style={inputBottomMargin}
+            name="lastName"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.lastName}
+            placeholder="Last name e.g. Trout"
+          />
+          {errors.lastName && touched.lastName && (
+            <Tag style={tagStyle}>{errors.lastName}</Tag>
+          )}
+          <Input
+            style={inputBottomMargin}
+            name="battingAverage"
+            type="Batting Average"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.battingAverage}
+            placeholder='Batting Average eg. "300"'
+          />
+          {errors.battingAverage && touched.battingAverage && (
+            <Tag style={tagStyle}>{errors.battingAverage}</Tag>
+          )}
+          <Input
+            style={inputBottomMargin}
+            name="playerType"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.playerType}
+            placeholder='Player Type eg. "HITTER" or "PITCHER"'
+          />
+          {errors.playerType && touched.playerType && (
+            <Tag style={tagStyle}>{errors.playerType}</Tag>
+          )}
+          <Button
+            onClick={() => submitForm()}
+            type="submit"
+            disabled={isSubmitting | (touched && !isValid)}
+          >
+            Submit
+          </Button>
         </form>
-    )}
+      )}
     </Formik>
-);
-
+  );
+};
 
 export default AddPlayerForm;
